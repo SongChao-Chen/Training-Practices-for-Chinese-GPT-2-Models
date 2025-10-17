2f9d38148abd398aeae296eb10adfd3c63fd8d2f
scchen <953776260@qq.com>
2025-10-17 10:08:06 +0800

    upload modified version

---
+++ README.md
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:3af3b88a564a7e658412372506c985c9b0dbfcb27fb3cb3bc4db9b40debd9206
+size 6239
---
+++ train.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:82279452dd43d30aeecdc8c21bf1a6b8a05685ffc376eb0817333ba2ad53cf43
+size 12368
